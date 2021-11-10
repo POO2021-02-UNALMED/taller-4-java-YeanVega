7c5224d16d3ecb249055619081a4afee7909b272
Yeferson Andres Velazco Galvis <92173102+YeanVega@users.noreply.github.com>
2021-11-09 21:23:33 -0500

    Update Asignatura.java

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -2,17 +2,13 @@ package classroom;
 
 public class Asignatura {
 
-    String nombre;
+	String nombre;
     String n = nombre;
     int codigoInterno;
     int codigoExterno;
 
     Asignatura() {
-        this("Sin nombre");
-    }
-
-    Asignatura(int codigoInterno) {
-        this("Sin nombre", codigoInterno, 0);
+        this(0);
     }
 
     Asignatura(int codigoExterno) {
@@ -23,17 +19,18 @@ public class Asignatura {
         this(nombre, 0, 0);
     }
 
-   
+    Asignatura(String nombre, int codigoInterno, int codigoExterno) {
+        this.nombre = nombre;
+        this.codigoInterno = codigoInterno;
+        this.codigoExterno = codigoExterno;
+    }
 
     void cambiarDatos(int codigoInterno, int codigoExterno, String nombre) {
         this.codigoInterno = codigoInterno;
         this.codigoExterno = codigoExterno;
         this.nombre = nombre;
-    
-
-    void cambiarDatos(int codigoInterno) {
-        this.codigoInterno = codigoInterno;
     }
+
 
     void cambiarDatos(int codigoExterno) {
         this.codigoExterno = codigoExterno;
@@ -42,8 +39,9 @@ public class Asignatura {
     void cambiarDatos(String nombre) {
         this.nombre = nombre;
     }
-    
-    public void setCodigoInterno(double codigoInterno) {
-        this.codigoInterno = (int) codigoInterno;
+
+    public void setCodigoInterno(int codigoInterno) {
+        this.codigoInterno = codigoInterno;
     }
+    
 }
