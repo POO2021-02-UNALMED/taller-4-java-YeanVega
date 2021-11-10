879342e680918aa40f51531bc3484c2c6c3fc37c
Yeferson Andres Velazco Galvis <92173102+YeanVega@users.noreply.github.com>
2021-11-09 21:25:19 -0500

    Update Persona.java

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -2,15 +2,20 @@ package classroom;
 
 public class Persona {
 
-    final long cedula;
+	long cedula;
     String nombre;
     static int totalPersonas;
     
     static {
         totalPersonas = 0;
-        cedula = 3;
     }
-
+    
+    Persona(){
+        this.cedula = 0;
+        this.nombre = "";
+        totalPersonas++;
+    }
+    
     Persona(long cedula, String nombre) {
         this.cedula = cedula;
         this.nombre = nombre;
@@ -30,7 +35,8 @@ public class Persona {
     }
 
     Persona(String nombre) {
-        this.nombre = "";
+    	this.cedula = 1;
+        this.nombre = nombre;
         totalPersonas++;
     }
     
@@ -46,3 +52,4 @@ public class Persona {
         this.nombre = nombre;
     }
 }
+
